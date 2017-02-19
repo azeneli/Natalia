9b6d5f62902635f2a4398f3a987b07ae94747652
Anri Zeneli <anri@seccosquared.com>
2017-02-19 14:48:59 -0500

    initial commit

---
+++ js/main.js
@@ -0,0 +1,35 @@
+(function() {
+  $(function() {
+    $("#fullpage").fullpage({
+      anchors: ["firstPage", "secondPage", "thirdPage", "fourthPage", "fifthPage"],
+      slidesColor: ["#000", "#000", "#000", "#000", "#f19539"],
+      slidesNavigation: true,
+      scrollingSpeed: 700,
+      keyboardScrolling: true,
+      touchSensitivity: 10,
+      continuousVertical: false,
+      animateAnchor: true,
+      afterLoad: function(anchorLink) { 
+        if (anchorLink === "thirdPage") {
+          $(".magic").addClass("animated bounceInLeft");
+          $(".trophy").addClass("animated bounceInUp");
+          return $(".users").addClass("animated bounceInRight");
+        }
+      }
+    });
+    $("#menu").click(function() {
+      $('.menu-bars').toggleClass("open");
+      return $(".overlay").toggleClass("open");
+    });
+    return $(".overlay ul li a").click(function() {
+      $(".overlay").removeClass("open");
+      return $('.menu-bars').removeClass("open");
+    });
+  });
+
+}).call(this);
+
+// #00a4e0
+// #DE564B 
+// #00a4e0  // first slide
+// default hex # for slide 2;
